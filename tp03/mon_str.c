3ee9386377902977af69e4c96359510839b599c9
asey <fr.kozlov.antoine@gmail.com>
2019-09-24 10:09:20 +0200

    Oui

--- tp03/mon_str.c
+++ tp03/mon_str.c
@@ -5,28 +5,28 @@ int mon_strlen(const char *s)
 {
 	const char *sc;
 
-	for(sc = s ; *sc != '\0' ; ++sc);
+	for(sc = s ; *sc != '\0' ; sc++);
 	return sc - s;
 }
 
 int mon_strcmp(const char *cs, const char *ct)
 {
-	unsigned char c1, c2;
+	char c1, c2;
+	c1 = *cs++;
+	c2 = *ct++;
 
-	while (1) {
+	while (c1 != '\0' && c2 != '\0') {
 		c1 = *cs++;
 		c2 = *ct++;
 		if (c1 != c2)
 			return c1 < c2 ? -1 : 1;
-		if (!c1)
-			break;
 	}
 	return 0;
 }
 
 int mon_strncmp(const char *cs, const char *ct, int count)
 {
-	unsigned char c1, c2;
+	char c1, c2;
 
 	while (count) {
 		c1 = *cs++;
@@ -44,7 +44,7 @@ char *mon_strcat(char *dest, const char *src)
 {
 	char *tmp = dest;
 
-	while (*dest)
+	while (*dest != '\0')
 		dest++;
 	while ((*dest++ = *src++) != '\0');
 	return tmp;
@@ -52,7 +52,7 @@ char *mon_strcat(char *dest, const char *src)
 
 char *mon_strchr(char *s, int c)
 {
-	for (; *s != (char)c; ++s)
+	for (; *s != (char)c; s++)
 		if (*s == '\0')
 			return NULL;
 	return (char *)s;
@@ -67,9 +67,9 @@ char *mon_strstr(const char *s1, const char *s2)
 		return (char *)s1;
 	l1 = mon_strlen(s1);
 	while (l1 >= l2) {
-		l1--;
 		if (!mon_strncmp(s1, s2, l2))
 			return (char *)s1;
+		l1--;
 		s1++;
 	}
 	return NULL;
